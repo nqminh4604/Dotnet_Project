aa473e0cfc77671d5a716a69b9b404a2e9d99dfe
nqminh4604 <153005744+nqminh4604@users.noreply.github.com>
2025-06-09 18:32:42 +0700

    link fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,24 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Dotnet_Project.Models;
+using Dotnet_Project.Data;
 
 namespace Dotnet_Project.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly DotnetProjectDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, DotnetProjectDbContext context)
     {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
         _logger = logger;
     }
 
     public IActionResult Index()
     {
+        //var courses = _context.Courses.ToList();
         return View();
     }
 
